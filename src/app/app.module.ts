20a29e35df36e9c1efc22eb064a0838f16a012b1
Kristi Laur <kristi@heisi.eu>
2022-05-04 14:39:11 +0300

    Set up and load Routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,15 @@ import { UserComponent } from './users/user/user.component';
 import { EditServerComponent } from './servers/edit-server/edit-server.component';
 import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
+import { RouterModule, Routes } from '@angular/router';
+
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'users', // localhost:4200/users
+    component: UsersComponent // when the abovementioned path is reached, which component should be loaded
+  },
+  { path: 'servers', component: ServersComponent }
+];
 
 @NgModule({
   declarations: [
@@ -25,6 +34,8 @@ import { ServersService } from './servers/servers.service';
   imports: [
     BrowserModule,
     FormsModule,
+    RouterModule.forRoot(appRoutes) // nüüd on meie ülal defineeritud appRoutes registreeritud ja meie app
+    // tunneb seal defineeritut ja oskab kasutada seda.
   ],
   providers: [ServersService],
   bootstrap: [AppComponent]
