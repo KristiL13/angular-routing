0530fef456fbc0068f9101eae323b2f1f1e2c2ab
Kristi Laur <kristi@heisi.eu>
2022-05-04 15:57:18 +0300

    Fetch Route parameters reactively

--- src/app/users/user/user.component.ts
+++ src/app/users/user/user.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-user',
@@ -17,6 +17,22 @@ export class UserComponent implements OnInit {
       // params propertytele, mis ma defineerisin Routes all.
       name: this.route.snapshot.params['name']
     }
+    // snapshoti kasutamise tõttu: Kui juba olen selle komponendi peal, mis
+    // peaks saama laetud, siis Angular ei lae seda uuesti ja sisu ei uuene.
+    // Angular üritab olla kokkuhoidlik.
+    // this.route.params on Observable, mis võimaldab jälgida asünkroonseid toiminguid.
+    this.route.params.subscribe(
+      // first parameter of subscribe: fired whenever the parameters change (in this use case)
+      // params will be an object that will hold the parameters as I defined them as properties.
+      (params: Params) => {
+        this.user.id = params['id'];
+        this.user.name = params['name'];
+      }
+    );
+    // Kui ma tean, et minu komponenti ei üritata kunagi laadida selle komponendi enda seest uuesti,
+    // siis on täiesti okei kasutada ainult snapshoti lahendust ka.
+    // Muul juhul, kasuta kindlasti subscribe varianti, et uuendada infot vastavalt mu
+    // route parameetrite muutumisele.
   }
 
 }
