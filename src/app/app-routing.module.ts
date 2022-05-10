de74ad874ba8129eba5a5184c32e7a326211c90c
Kristi Laur <kristi@heisi.eu>
2022-05-10 08:12:27 +0300

    Understanding location strategies: using hash in routes
    What to do if hosting server tries to do too much with the route paths and
    gives 404 and doesn't redirect to index.html.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -64,6 +64,17 @@ const appRoutes: Routes = [
   imports: [
     RouterModule.forRoot(appRoutes), // nüüd on meie ülal defineeritud appRoutes
     // registreeritud ja meie app tunneb seal defineeritut ja oskab kasutada seda.
+
+    // RouterModule.forRoot(appRoutes, { useHash: true }),
+    // useHash: true -- vajalik, et server ei otsiks neid route backist vaid laseks
+    // Angularil frontis nendega majandada. Muidu kui backist seda rada ei leia,
+    // siis saab 404 veateate lehe. Vanemad brauserid võivad ka mitte toetada kuidas
+    // Angular tahaks toimida. Selleks peame käsitsi siin selle useHash asja panema,
+    // et 404 korral suunataks index.html lehele tagasi.
+    // Siis route muutub http://host.server(localhost või misiganes)/#/servers nt ja
+    // hostiv server ignoreerib kõike, peale #, ehk ta ei hakka kusagilt neid lõppusid
+    // otsima backis ega nii. Pärast räägib ka, kuidas on parem lähenemine serveri
+    // confimiseks, et suunaks 404 korral index.html peale.
   ],
   // Määrab, et kui ma impordin siinse mooduli teise moodulisse, et mis peaks siit
   // moodulist olema kättesaadav seal, kuhu siinset impordin.
